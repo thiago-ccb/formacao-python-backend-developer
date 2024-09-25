77ebe8dbdeca3b57a280aa714aa90e6541ed008c
Thiago Oliveira <thiagosoaresccb@gmail.com>
2024-09-25 01:52:24 -0300

    gerenciamento de pacotes, convencoes e boas praticas

---
+++ 01 - Gerenciamento de Pacotes, Convencoes e Boas Praticas Python/boas_praticas/boas_praticas.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+frutas = [
+    "laranja", 
+    "melão", 
+    "banana", 
+    "melancia", 
+    "pera", 
+    "maçã",
+    "uva"
+    ]
+
+carros = ["polo", "hb20", "bmw"] 
